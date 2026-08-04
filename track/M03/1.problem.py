848c0095831f0c783910a8a00c799769c4c7e23a
Dhanush <dhanushgt1@gmail.com>
2026-08-04 19:45:38 +0530

    changes

--- track/M03/1.problem.py
+++ track/M03/1.problem.py
@@ -1,24 +1,10 @@
-#student_name= str(input())
-#student_age= int(input())
-#student_percentage= int(input())
+student_name= str(input())
+student_age= int(input())
+student_percentage= int(input())
 
-#print(type(student_name))
-#print(type(student_age))
-#print(type(student_percentage))
+print(type(student_name))
+print(type(student_age))
+print(type(student_percentage))
 
 
 
-#first_number= int(input())
-#second_number= int(input())
-
-#print(first_number + second_number)
-
-#celsius = float(input())
-#Fahrenhiet= (celsius*9/5)+32
-#print(Fahrenhiet)
-
-total_bill= float(input())
-people= int(input())
-
-amount= total_bill/people
-print(amount)
